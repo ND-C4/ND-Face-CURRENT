e669fd6f1e125bc7934103ca9f350f1eb66788c0
ndkurogo <mobile@nd.edu>
2015-05-19 12:11:13 -0400

    5-12-15 C[4] Meeting
    foo

---
+++ NDFace/EnrollmentConfirmationViewController.h
@@ -0,0 +1,16 @@
+//
+//  EnrollmentConfirmationViewController.h
+//  NDFace
+//
+//  Created by Matt Willmore on 5/12/15.
+//  Copyright (c) 2015 Architecture Information Technology Team. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface EnrollmentConfirmationViewController : UIViewController {
+
+IBOutlet UILabel *EnrollmentConfirmationLabel;
+
+}
+@end
